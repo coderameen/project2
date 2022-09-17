196b841f4f204e266121fa4872900c08fd595223
coder ameen <73511997+coderameen@users.noreply.github.com>
2022-09-17 15:45:09 +0530

    Add files via upload
    React Todo files Added inpin

---
+++ Plan.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Plan(props){
+  return(<>
+  <div className= "row mt-3">
+    <div className="col-8">
+    <li>{props.value}</li>
+    </div>
+    <div className="col-4">
+    <button className="btn btn-danger" onClick={()=>{props.sendFunc(props.id)}}>Delete</button>
+    </div>
+    </div>
+    </>
+  )
+}
+export default Plan;
